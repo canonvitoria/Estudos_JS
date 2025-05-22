000d7fcd17c75d0328527cf6eecf8c83ed33f1cd
Vitória Canon <98993613+canonvitoria@users.noreply.github.com>
2025-05-22 19:16:59 -0300

    Delete CursoEmVídeo directory

--- CursoEmVídeo/Aula-11/Exercício008.js
+++
@@ -1,8 +0,0 @@
-var velocidade = 78.2
-console.log(`A velociade do seu carro é de ${velocidade}Km/h`)
-
-if (velocidade >= 60) {
-    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
-}
-
-console.log(`Dirija smepre usando cinto de segurança!`)
